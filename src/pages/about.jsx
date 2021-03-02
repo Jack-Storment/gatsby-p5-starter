30d4e6b6c655744b48a2ed8faa8cae4f29595c7a
jack-storment <jstorment1998@gmail.com>
2021-03-02 10:50:29 -0500

    updates

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,9 +19,11 @@ const About = ({ data }) => {
         <p>Started a web development business in Durham, North Carolina that was profitable in under two weeks.</p>
         <h1>Professional Experience</h1>
         { data && nodes && <Img className="round" fluid={ nodes[0].childImageSharp.fluid } /> }
-        <p>My name is Wendy Dherin, and I&rsquo;m a full stack developer with a passion for making beautiful audio and visual artworks and building physics engines with <a href="https://p5js.org/" target="_blank" rel="noreferrer">p5.js</a>.</p>
-        <p>In 2014 I built <a href="https://www.audiosnowflake.com" target="_blank" rel="noreferrer">Audio Snowflake</a>, a music-visualizer that creates unique spirographs for songs based on their musical properties, using a predecessor of p5.js, <a href="https://www.processing.org/" target="_blank" rel="noreferrer">Processing</a>. <strong><i>Update on Audio Snowflake:</i></strong> It&rsquo;s currently not fully functional because the Echonest API it relies on has gone the way of the dodo; only songs that are already in the database are searchable. So you can try to search and you might get lucky and get a snowflake, otherwise, search just on the artists, and you might get a different song by them. I hope to soon update the app to use the Spotify API, which has replaced the Echonest one.</p>
-        <p>I&rsquo;m originally from California, but I currently reside in Ireland.</p>
+        <p>In 2015 I started my web development career by helping small business owner Arno Ilgner build a website that allowed for the global sale of his books translated into three different languages via custom Wordpress and Shopify development.</p>
+        <p>In 2016 I built <a href="https://triangleweb.solutions" target="_blank" rel="noreferrer">Triangle Web Solutions</a>, a web development company mainly focused on React that empowers local businesses in the Research Triangle Park area to help build a strong web presence. This has been especially helpful to local resturaunts by enabling contact-less pickup. One of our biggest clients, Matt Ganzert, was not only able to survive the pandemic but thrive as he went on to start two more resturaunts in the area! </p>
+        <p>In 2017 I interned at Fidelity Investments to help redesign their landing page as well as help with data visualization projects.</p>
+        <p>Currently, I am still running Triangle Web Solutions although we are working with our last client as my business partner just accepted a job at Facebook.</p>
+        <p>After I graduated from UNC, I accepted a one year contract job at QuestBlue Systems building multiple enterprise mobile and web telecommunications applications for our clients such as Penn State University. The contract ends in March of 2021 and I am currently looking for my next full-time job opportunity!</p>
       </div>
     </Layout>
   )
