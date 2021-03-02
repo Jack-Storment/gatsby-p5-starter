e8089ac016474f91744635b358468393ca99ec38
jack-storment <jstorment1998@gmail.com>
2021-03-02 10:37:36 -0500

    updates

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
     <Layout>
       <div className="contact-content-container">
         <h1>Contact Me</h1>
-        <p>My email address is my first name followed by my last name at gmail. Let me know if you have any questions or comments about the site -- I&rsquo;d be delighted to have your feedback.</p>
+        <p>My email address is my last name followed by my initials "JD" at gmail. Let me know if you would like to connect about any job or project you have available. I can also be reached at 919-508-4830.</p>
       </div>
     </Layout>
   )
